1232b50454f4a106f4a6831caabec0e52540f8f5
Marina <mgdelneri@gmail.com>
2021-05-10 09:36:27 -0500

    Added router.delete code to product-router.js in api folder

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -113,6 +113,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = Product.destroy({
+      where: {
+        id: req.params.id,
+      }
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    };
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
